08b68b90f4e18c1e859f7c08ab9b42b1587caec3
DANG QUOC ANH <quocanh.krongno@gmail.com>
2022-11-29 20:57:17 +0700

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,18 @@
 window.onscroll = function () {
-  let header = document.getElementById('header-lg')
-  let brand = document.getElementById('nav__brand')
+  let header = document.getElementById("header-lg");
+  let brand = document.getElementById("nav__brand");
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    header.classList.add('scrolled')
-    brand.style.visibility = 'hidden'
-    brand.style.height = '50px'
+    header.classList.add("scrolled");
+    brand.style.visibility = "hidden";
+    brand.style.height = "50px";
   } else {
-    header.classList.remove('scrolled')
-    brand.style.visibility = 'visible'
-    brand.style.height = 'auto'
+    header.classList.remove("scrolled");
+    brand.style.visibility = "visible";
+    brand.style.height = "auto";
   }
-}
+};
+
+document.getElementById("sidebar-toggler").onclick = () => {
+  document.getElementById("nav__sidebar").classList.toggle("sidebar-on");
+  document.getElementById("nav__header").classList.toggle("sidebar-on");
+};
